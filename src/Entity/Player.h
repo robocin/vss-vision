affc736040f3f809cce87b807e08858d0c403424
Lucas Oliveira Maggi <lom@cin.ufpe.br>
2019-07-24 16:13:16 -0300

    Cleaned Player.

--- src/Entity/Player.h
+++ src/Entity/Player.h
@@ -4,16 +4,6 @@
 #include "Entity.h"
 
 class Player : public Entity {
-  Float m_angle;
-
-  Void updateAngle(const Float &t_angle);
-
-public:
-  Player();
-
-  Void update(const Float &t_time, const Point &t_position, const Float &t_angle);
-
-  Float angle();
 };
 
 #endif // PLAYER_H
