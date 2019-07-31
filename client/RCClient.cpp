3eb36a01491258a87747adfe16f00d515b0413fb
Lucas Oliveira Maggi <lom@cin.ufpe.br>
2019-07-31 15:57:00 -0300

    Updated Makefile and RCClient to run with OMP to use sections to have an graphic interface and C++11 to use mutex.

--- client/RCClient.cpp
+++ client/RCClient.cpp
@@ -2,6 +2,10 @@
 #include <SFML/Network.hpp>
 #include <iostream>
 #include <vector>
+#include <mutex>
+#include <atomic>
+#include <omp.h>
+
 
 typedef struct s_Entity {
     int id;
@@ -11,53 +15,69 @@ typedef struct s_Entity {
 typedef std::vector<Entity> Entities;
 
 Entities entities;
+std::mutex mtx;
+std::atomic<bool> Exit;
 
 int main()
 {
-    // UDP socket:
-    sf::UdpSocket socket;
-    sf::IpAddress sender = "0.0.0.0";
-    sf::Uint16 oport = 54000, port = oport;
-
-    // bind the socket to a port
-    if (socket.bind(port) != sf::Socket::Done)
+    #pragma omp parallel sections
     {
-        fprintf(stderr,"Error trying to bind socket on port %d\n", port);
-    }
-    while (true) {
-        sf::Packet packet;
-        port = oport;
-        if (socket.receive(packet, sender, port) != sf::Socket::Done)
+        #pragma omp section 
         {
-            // error...
-        }
-        std::cout << "Received from " << sender << " on port " << port << std::endl;
-        sf::Uint8 entitiesSize;
-        if (packet >> entitiesSize) {
-            std::cout << "entities : " << static_cast<int>(entitiesSize) << std::endl;
-        } else {
-            std::cout << "Error on retrieving data" << std::endl;
-        }
-    }
-    /*
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+            // UDP socket:
+            sf::UdpSocket socket;
+            sf::IpAddress sender = "0.0.0.0";
+            sf::Uint16 oport = 54000, port = oport;
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
+            // bind the socket to a port
+            if (socket.bind(port) != sf::Socket::Done)
+            {
+                fprintf(stderr,"Error trying to bind socket on port %d\n", port);
+            }
+            while (!Exit) {
+                sf::Packet packet;
+                port = oport;
+                if (socket.receive(packet, sender, port) != sf::Socket::Done)
+                {
+                    // error...
+                }
+                std::cout << "Received from " << sender << " on port " << port << std::endl;
+                sf::Uint8 entitiesSize;
+                if (packet >> entitiesSize) {
+                    std::cout << "entities : " << static_cast<int>(entitiesSize) << std::endl;
+                    mtx.lock();
+                    entities.resize(static_cast<int>(entitiesSize));
+                    mtx.unlock();
+                } else {
+                    std::cout << "Error on retrieving data" << std::endl;
+                }
+            }
+        }
+
+        #pragma omp section 
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+            sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+            sf::CircleShape shape(100.f);
+            shape.setFillColor(sf::Color::Green);
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+            while (window.isOpen())
+            {
+                sf::Event event;
+                while (window.pollEvent(event))
+                {
+                    if (event.type == sf::Event::Closed) {
+                        window.close();
+                        Exit = true;
+                    }
+                }
+
+                window.clear();
+                window.draw(shape);
+                window.display();
+            }
+        }
     }
-    */
+    
 
     
 
