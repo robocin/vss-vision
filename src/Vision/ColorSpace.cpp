845efedb798248d8dcd8b7a9d286a2364fec8c21
Lucas Oliveira Maggi <lom@cin.ufpe.br>
2021-04-15 23:31:25 -0300

    [CHANGED] marker colors to extrapolated first 8 colors [black/no color to light blue/cyan].

--- src/Vision/ColorSpace.cpp
+++ src/Vision/ColorSpace.cpp
@@ -3,6 +3,23 @@
 RGB ColorSpace::markerColors[NUMBEROFCOLOR] = {
         //order: blue, green, red
         {0,0,0},//NoCOL
+        {0,128,255},//OrangeCOL
+        {255,0,0},//BlueCOL
+        {0,255,255},//YellowCOL
+        {0,0,255},//RedCOL
+        {0,255,0},//GreenCOL
+        {255,0,255},//PinkCOL
+        {255,255,0},//LightBlueCOL
+        {191,15,179},//PurpleCOL
+        {25,0,51},//BrownCOL
+        {0,0,0}//ColorStrange
+};
+
+// LEGACY
+/*
+RGB ColorSpace::markerColors[NUMBEROFCOLOR] = {
+        //order: blue, green, red
+        {0,0,0},//NoCOL
         {20,147,255},//OrangeCOL
         {255,0,0},//BlueCOL
         {0,255,255},//YellowCOL
@@ -13,4 +30,4 @@ RGB ColorSpace::markerColors[NUMBEROFCOLOR] = {
         {191,15,179},//PurpleCOL
         {25,0,51},//BrownCOL
         {0,0,0}//ColorStrange
-};
+};*/
