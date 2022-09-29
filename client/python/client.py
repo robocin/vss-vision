3a7e00beb74d6318e9bd7457584d6773eec74f44
bmmuc <bmmuc@cin.ufpe.br>
2022-09-29 16:14:57 -0300

    feat(client): adding stuff breno asked

--- client/python/client.py
+++ client/python/client.py
@@ -1,9 +1,22 @@
 import socket
 import struct
+from time import time
+import wrapper_pb2 as wr
+import messages_robocup_ssl_detection_pb2 as detection_t
+import messages_robocup_ssl_geometry_pb2 as geo
 
-class VSSClient:
-    def __init__(self):
-        pass
+class PROTOClient:
+    def __init__(self, ip='127.0.0.1', port=20011):    
+        self.com_ip = ip
+        self.com_port = port
+        self.com_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.com_address = (self.com_ip, self.com_port)
+
+    def fix_x(self, x):
+        return (x * 10) - 75
+    
+    def fix_y(self, y):
+        return (y * 10) - 65
 
     # Returns a list of entities
     def unpack(self,data):
@@ -18,30 +31,87 @@ class VSSClient:
             sock = socket.socket(socket.AF_INET, # Internet
                 socket.SOCK_DGRAM) # UDP
             sock.bind((UDP_IP, UDP_PORT))
-            print("VSSClient :: listening on port {}\n".format(UDP_PORT))
+            print("PROTOClient :: listening on port {}\n".format(UDP_PORT))
+            # pkt_create_to_send = ref_placement_pb2.VSSRef_Placement()
+
+            # robots = pkt_create_to_send.world.robots
+
+            # for id, rbt in robots_dict.items():
+                # _robot = robots.add()
+            # detection = wr.SSL_WrapperPacket.detection
+            # print(dir(detection))
             while True:
+                msg = wr.SSL_WrapperPacket()
+                frame = msg.detection
+                # frame = detection.SSL_DetectionFrame()
+                ball = frame.balls.add()
+                robots_blue = frame.robots_blue
+                robots_yellow = frame.robots_yellow
+
                 data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
                 messageType = struct.unpack_from('c',data,0)[0].decode("utf-8")
                 print("message type {}".format(messageType))
                 if (messageType != 'F'): continue
+
                 timestamp = struct.unpack_from('>I',data,1)[0]
                 frameId = struct.unpack_from('>i',data,5)[0]
+
+                frame.frame_number = frameId
+                frame.t_capture = timestamp
+                frame.t_sent = timestamp
+                frame.camera_id = 0
                 entities = ord(struct.unpack_from('c',data,header_offset-1)[0])
                 print("message '{}' have {} entities on frame {} time:{}".format(messageType,entities,frameId,timestamp))
-                for i in range(0,entities):
+
+                id = ord(struct.unpack_from('c',data,header_offset + entity_size*0)[0])
+                x, y, ang = struct.unpack_from('ddd',data,header_offset+1 + entity_size*0)
+                ball.x = self.fix_x(x)
+                ball.y = self.fix_y(y)
+                ball.pixel_x = ball.x * 10
+                ball.pixel_y = ball.y * 10
+                ball.confidence = 1
+                print(entities)
+                for i in range(1,4):
                     id = ord(struct.unpack_from('c',data,header_offset + entity_size*i)[0])
-                    # print("id {} ".format(id))
                     x, y, ang = struct.unpack_from('ddd',data,header_offset+1 + entity_size*i)
-                    print("id {} position ({},{},{})".format(id,x,y, ang))
+                    _robot = robots_blue.add()
+                    _robot.robot_id = id
+                    _robot.x = x
+                    _robot.y = y
+                    _robot.confidence = 1.0
+                    _robot.pixel_x = _robot.x * 10
+                    _robot.pixel_y = _robot.y * 10
+
+                for i in range(4, entities):
+                    id = ord(struct.unpack_from('c',data,header_offset + entity_size*i)[0])
+                    x, y, ang = struct.unpack_from('ddd',data,header_offset+1 + entity_size*i)
+                    _robot = robots_yellow.add()
+                    _robot.robot_id = id
+                    _robot.x = x
+                    _robot.y = y
+                    _robot.confidence = 1.0
+                    _robot.pixel_x = _robot.x * 10
+                    _robot.pixel_y = _robot.y * 10
+
+
+
+                data = msg.SerializeToString()
+                # msg.SerializeToString()
+                # msg.SSL_DetectionFrame = detection
+                self.com_socket.sendto(data, self.com_address)
+
+                # print("id {} position ({},{},{})".format(id,x,y, ang))
 
         except KeyboardInterrupt:
-            print("\nVSSClient :: Client closed.")
+            print("\nPROTOClient :: Client closed.")
         finally:
             pass
 
-
+a = PROTOClient()
+a.run()
+# 75 e 65
 def print_about():
-    print("This is the VSSClient module.\nPlease use the VSSClient by importing this module into your code.")
+    print("This is the PROTOClient module.\nPlease use the PROTOClient by importing this module into your code.")
 
 if __name__ == '__main__':
     print_about()
