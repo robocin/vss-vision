ff13864bb98c62095f98a0c95ab19dd20f7baaad
acrucha <mcaa@cin.ufpe.br>
2024-11-25 00:19:34 -0300

    build: create stdc++, add json lib and absl to cmake

---
+++ include/bits/stdc++.h
@@ -0,0 +1,23 @@
+#ifndef _GLIBCXX_BITS_STDCPP_H
+#define _GLIBCXX_BITS_STDCPP_H
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <limits>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+
+#endif
