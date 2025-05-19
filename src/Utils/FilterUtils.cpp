3241a7c8c6617ed40fe9eeb829e2a39a60f1bb18
Thiago-Ramalho <trm4@cin.ufpe.br>
2025-05-18 21:02:35 -0300

    feat(Utils): create FilterUtils.cpp and FIlterUtils.h with rectangle saveFilterRectToJson function Co-authored-by: caio fernandes <cvmfc@cin.ufpe.br>

---
+++ src/Utils/FilterUtils.cpp
@@ -0,0 +1,23 @@
+#include "FilterUtils.h"
+
+void FilterUtils::saveFilterRectToJson(const std::vector<cv::Rect>& rects, const QString& path) {
+    QFile file(path);
+
+    QJsonArray rectsArray;
+    for (const auto& rect : rects) {
+        QJsonObject rectObj;
+        rectObj["x"] = rect.x;
+        rectObj["y"] = rect.y;
+        rectObj["width"] = rect.width;
+        rectObj["height"] = rect.height;
+        rectsArray.append(rectObj);
+    }
+
+    if (file.open(QIODevice::WriteOnly | QIODevice::Truncate | QIODevice::Text)) {
+        QJsonDocument saveDoc(rectsArray);
+        file.write(saveDoc.toJson(QJsonDocument::Indented));
+        file.close();
+    } else {
+        qWarning() << "Não foi possível abrir" << path << "para escrita!";
+    }
+}
